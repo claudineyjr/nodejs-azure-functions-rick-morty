320a14163b7a3360bcf708145b76df1560879a8c
Claudiney Junior <claudiney.alves@fatec.sp.gov.br>
2019-05-10 17:59:10 -0300

    Endpoint de personagens

--- personagens/index.js
+++ personagens/index.js
@@ -1,16 +1,17 @@
+global.fetch = require('node-fetch');
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+    return fetch('https://rickandmortyapi.com/api/character')
+        .then(respo => respo.json())
+        .then((resposta) =>{
+            context.res = { body: resposta };
+        }).catch((error) => {
+            context.log(error);
+            context.res = {
+                status: 500,
+                body: `An error has ocurred`
+            };
+        });
 };
